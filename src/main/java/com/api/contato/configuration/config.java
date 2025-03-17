c6a9b3d7dddf3d594bd0d1f1d0eb2dfe0dd7b43c
dyeggoSilva <dyeggo0102@live.com>
2025-03-17 02:35:08 -0300

    up

--- src/main/java/com/api/contato/configuration/config.java
+++ src/main/java/com/api/contato/configuration/config.java
@@ -4,13 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
-public class config implements WebMvcConfigurer{
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permite todas as URLs
-                .allowedOrigins("*") // Permite este domínio
-                .allowedMethods("GET", "POST") // Permite estes métodos
-                .allowedHeaders("*"); // Permite todos os headers
+public class config{
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permite todas as origens (não recomendado para produção)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
     }
 }
